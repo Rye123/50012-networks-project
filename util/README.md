268fcaa56f98dd153117d40bb4339e442f2f545f
Ryan Toh <ryedralisk@gmail.com>
2023-03-28 01:00:07 +0800

    feat: temp file storage and loading

--- util/README.md
+++ util/README.md
@@ -16,3 +16,13 @@ CRINFO {File Size} {Timestamp}<CRLF>
 
 ### Temp Files: `.crtemp`
 Stores the temporary data of each file.
+
+```
+CRTEMP {Block Count}<CRLF>
+{Block Pointers}<CRLF><CRLF>
+```
+- After the first line, the next few lines are the block pointers.
+- Each block pointer is a 4-byte signed integer pointing to the first byte of the block data later in the file.
+  - If the integer is `-1`, the block does not exist.
+
+Header is ended with `\r\n\r\n`.
--- util/files.py
+++ util/files.py
@@ -192,7 +192,7 @@ class File:
                 return False
         return True
 
-    def save_file(self, shared_dir: str=DEFAULT_SHARED_DIR):
+    def save_file(self, shared_dir: str=DEFAULT_SHARED_DIR) -> str:
         if not self.downloaded:
             raise FileError("save_file Error: File not fully downloaded.")
         
@@ -206,7 +206,38 @@ class File:
         ensure_shared_folder(shared_dir)
         write_file(path, data)
         logging.info(f"{self.fileinfo.filename} written to directory.")
+        return path
     
+    def save_temp_file(self, shared_dir: str=DEFAULT_SHARED_DIR) -> str:
+        if self.downloaded:
+            raise FileError("save_temp_file Error: File already fully downloaded.")
+
+        # Save the fileinfo
+        fileinfo = self.fileinfo
+        crinfo_file = fileinfo.save_crinfo(shared_dir)
+        logging.debug(f"CRINFO for {self.fileinfo.filename} saved as {crinfo_file}.")
+
+        block_pointers:List[bytes] = []
+        data = b'' 
+        for block in self.blocks:
+            if not block.downloaded:
+                block_pointers.append((-1).to_bytes(4, signed=True))
+            else:
+                # len(data) gives the first byte of this block
+                block_pointer = len(data).to_bytes(4, signed=True)
+                data += block.data
+                block_pointers.append(block_pointer)
+        
+        header_line_1 = f"CRTEMP {self.fileinfo.block_count}".encode('ascii')
+        header = header_line_1 + b'\r\n' + b'\r\n'.join(block_pointers)
+        full_data = header + b'\r\n\r\n' + data
+
+        path = f"{shared_dir}/{self.fileinfo.filename}.{self.TEMP_FILE_EXT}"
+        ensure_shared_folder(shared_dir)
+        write_file(path, full_data)
+        logging.info(f"{self.fileinfo.filename}.{self.TEMP_FILE_EXT} written to directory.")
+        return path
+
     @staticmethod
     def from_file(path: str) -> 'File':
         if path.endswith(File.TEMP_FILE_EXT):
@@ -233,5 +264,73 @@ class File:
             for i in range(fileinfo.block_count):
                 file.blocks[i].data = f.read(MAX_BLOCK_SIZE)
                 file.blocks[i].downloaded = True
+        
+        return file
+
+    @staticmethod
+    def from_temp_file(path: str) -> 'File':
+        if not path.endswith(File.TEMP_FILE_EXT):
+            raise ValueError("from_file: Invalid file (path is not a temp file)")
+        
+        pathobj = Path(path)
+        if not pathobj.is_file():
+            raise ValueError('from_file: Invalid file.')
+        
+        # Get FileInfo header, if not create one.
+        filedir = pathobj.parent
+        filename_orig = pathobj.name.removesuffix(f".{File.TEMP_FILE_EXT}")
+        
+        fileinfo_filename = f"{filedir}/{FileInfo.CRINFO_DIR_NAME}/{filename_orig}.{FileInfo.CRINFO_EXT}"
+        fileinfo = None
+        try:
+            fileinfo = FileInfo.from_crinfo(fileinfo_filename)
+        except FileNotFoundError:
+            raise ValueError("from_file: Given temp file has no corresponding CRINFO file.")
+
+        file = File(fileinfo)
+
+        # Process file
+        file_raw:bytes = b''
+        with pathobj.open('rb') as f:
+            file_raw = f.read()
+        
+        file_raw_split = file_raw.split(b'\r\n\r\n', 1)
+        header = file_raw_split[0].split(b'\r\n')
+        data = file_raw_split[1]
+
+        l1_split = header[0].decode('ascii').split(' ')
+        if len(l1_split) != 2:
+            raise FileError("from_file: Given temp file has invalid first line.")
+        
+        sig, block_count = l1_split[0], int(l1_split[1])
+        if sig != "CRTEMP":
+            raise FileError("from_file: Given temp file has invalid file signature.")
+        if block_count != len(header)-1:
+            raise FileError(f"from_file: Mismatch in block count {block_count} and number of block pointers {len(header)-1}")
+
+        # Process block pointers
+        block_data:List[bytes] = []
+        for i in range(1, len(header)-1):
+            first_bytepos = int.from_bytes(header[i], signed=True)
+            if first_bytepos == -1:
+                block_data.append(b'')
+            else:
+                last_bytepos = int.from_bytes(header[i+1], signed=True)
+                if last_bytepos == -1:
+                    block_data.append(data[first_bytepos:])
+                else:
+                    block_data.append(data[first_bytepos:last_bytepos]) 
+
+        # Process final block
+        first_bytepos = int.from_bytes(header[-1], signed=True)
+        if first_bytepos == -1:
+            block_data.append(b'')
+        else:
+            block_data.append(data[first_bytepos:])
+
+        # Set blocks
+        for i in range(block_count):
+            file.blocks[i].data = block_data[i]
+            file.blocks[i].downloaded = not (block_data[i] == b'')
         
         return file
